e2bb9f3b6c25a844bb255f0410ff4bac64ca9dcb
Eduardo Santos <eduardo.edusantos@gmail.com>
2014-09-08 01:30:10 -0300

    Primeiros imports de dados de município

--- test/server/municipios/import-sp.js
+++ test/server/municipios/import-sp.js
@@ -7,29 +7,106 @@ var should = require('should'),
     request = require('supertest'),
     fs = require('fs'),
     assert = require('assert'),
-    xmldom = require('xmldom');
+    xmldom = require('xmldom'),
+    mongoose = require('mongoose'),
+    Municipio = mongoose.model('Municipio');
 
-describe('GET /import/municipio/540251f1c28567662f27282d', function () {
+describe('GET /import/municipio/540bcaf454ccaf3175170623', function () {
 
-   it('Testa existência de arquivos de dados', function(done) {
-       assert.doesNotThrow(fs.readFile(__dirname+'/../../../data/saude-educacao-sp-2013.xml', function (err, data) {
-           if (err) {
-               console.log("Erro lendo o arquivo XML de dados original\n"+err);
-               throw err;
-           }
-       }));
+    it('Só pra ver se o teste está funcionando', function() {
+        var retval = true;
+
+        assert.equal(true, retval);
+    })
+
+   it('Testa existência de arquivos de dados', function() {
+       assert.doesNotThrow(function() {
+           fs.readFile(__dirname+'/../../../data/saude-educacao-sp-2013.xml', function (err, data) {
+               if (err) {
+                   console.log("Erro lendo o arquivo XML de dados original\n"+err);
+                   throw err;
+               }
+            });
+       });
    })
 
-   it('Testa parsing do arquivo XML de dados', function(done) {
+   it('Testa parsing do arquivo XML de dados', function() {
        fs.readFile(__dirname+'/../../../data/saude-educacao-sp-2013.xml', function (err, data) {
            if (err) {
                console.log("Erro lendo o arquivo XML de dados original\n"+err);
                throw err;
            }
-           var Dom = xmldom.DOMParser;
+           var DOM = xmldom.DOMParser;
            var doc;
-           assert.doesNotThrow(doc = new DOM().parseFromString(data.string));
+           assert.doesNotThrow(function() {
+               doc = new DOM().parseFromString(data.string);
+           });
        });
+   })
+
+   it('Encontra dados de saúde e educação dentro de um arquivo XML', function() {
+       fs.readFile(__dirname+'/../../../data/saude-educacao-sp-2013.xml', function (err, data) {
+           if (err) {
+               console.log("Erro lendo o arquivo XML de dados original\n"+err);
+               throw err;
+           }
+           var DOM = xmldom.DOMParser;
+           var doc;
+           var despesas;
+           var saude;
+           var educacao;
+
+           doc = new DOM().parseFromString(data.toString(), 'text/xml');
+           despesas = doc.getElementsByTagName('Descrição_Função2');
+           saude = despesas[0].getAttribute('Textbox35');
+           educacao = despesas[1].getAttribute('Textbox35');
+           assert.equal(saude, '5963902681.7');
+           assert.equal(educacao, '8111898059.3');
+       });
+
+
+   })
+
+   it('Carrega dados do Município', function(done) {
+       request(app)
+           .get('/api/municipios?id=540bcaf454ccaf3175170623')
+           .expect(200)
+           .expect('Content-Type', /json/)
+           .end(function(err, res) {
+               if (err) return done(err);
+               res.body.should.be.instanceof(Array);
+               //console.log(JSON.parse(res.body));
+               if(res.body[0].nome == 'SAO PAULO' &&
+                   res.body[0].cod == '7107' &&
+                   res.body[0].uf == 'SP')
+               //res.body[0]._id == '5353fd784d2bc7ed64289af7' &&
+               //res.body[0].__v == 0)
+                   console.log('Município encontrado com sucesso');
+               done();
+           });
+   })
+
+   it("Adiciona dados de saúde no Município selecionado", function(done) {
+       request(app)
+           .get('/import/municipios/540bcaf454ccaf3175170623')
+           .expect(200)
+           .expect('Content-Type', /json/)
+           .end(function(err, res) {
+               if (err) return done(err);
+               res.body.should.be.instanceof(Array);
+               //console.log(JSON.parse(res.body));
+               if(res.body[0].nome == 'SAO PAULO' &&
+                   res.body[0].cod == '7107' &&
+                   res.body[0].uf == 'SP')
+               //res.body[0]._id == '5353fd784d2bc7ed64289af7' &&
+               //res.body[0].__v == 0)
+                   console.log('Município encontrado com sucesso');
+
+               municipio = res.body[0];
+               assert.equal(municipio.saude, '5963902681.7');
+               assert.equal(municipio.educacao, '8111898059.3');
+               done();
+           });
    })
 
 });
